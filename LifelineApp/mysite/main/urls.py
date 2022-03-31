46c7760c93cca4372d108a2e57c1c0676ab1a7d1
soliseyoum <soliana.seyoum@bison.howard.edu>
2022-03-31 09:44:08 -0400

    Modified html files to connect with middle tier

---
+++ LifelineApp/mysite/main/urls.py
@@ -0,0 +1,34 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+#from django.contrib import admin
+from . import views
+from django.urls import path
+from .views import HomePageView, AddPageView, ProfilePageView, LogInPageView, LoggedInPageView
+
+
+
+app_name = "main"
+
+urlpatterns = [
+    path("loggedin/", LoggedInPageView.as_view(), name="loggedin"),
+    path("login/", LogInPageView.as_view(), name="login"),
+    path("profile/", ProfilePageView.as_view(), name="profile"),
+    path("add/", AddPageView.as_view(), name="add"),
+    path("", HomePageView.as_view(), name="home"),
+
+    #path("", views.index, name ="homepage"),
+   # path("", views.add, name ="add"),
+]
---
+++ LifelineApp/mysite/main/views.py
@@ -0,0 +1,90 @@
+from re import template
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+import pyrebase
+
+# Create your views here.
+#def homepage(request):
+#    return HttpResponse("Wow")
+
+firebaseConfig = {
+  "apiKey": "AIzaSyD3b8GXxpcjGhI-2nBVQM2hu_3JUpLj-uM",
+  "authDomain": "seniorproject-husp22.firebaseapp.com",
+  "databaseURL": "https://seniorproject-husp22-default-rtdb.firebaseio.com",
+  "projectId": "seniorproject-husp22",
+  "storageBucket": "seniorproject-husp22.appspot.com",
+  "messagingSenderId": "759118984872",
+  "appId": "1:759118984872:web:0d5ff96057621826e3749d",
+  "measurementId": "G-MVNFP9QRN2"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+db = firebase.database()
+auth = firebase.auth()
+storage = firebase.storage()
+
+def index(request):
+  #accessing our firebase data and storing it in a variable
+  medicine = db.child('medicine').child('1').child("name").get().val()
+  directions = db.child('medicine').child('1').child("directions").get().val()
+  schedule = db.child('medicine').child('1').child("schedule").get().val()
+
+  #medicine = db.child("people").order_by_child("age").start_at(30).get()
+  #for med in medicine.each():
+    # print(med.val()["name"])
+
+  context = {
+      'medicine':medicine,
+      'directions':directions,
+      'schedule':schedule
+  }
+  return render(request, 'index.html', context)
+
+def add(request):
+    return render(request, 'add.html')
+
+class HomePageView(TemplateView):
+  template_name = "index.html"
+
+class AddPageView(TemplateView):
+  template_name = "add.html" 
+
+class ProfilePageView(TemplateView):
+  template_name = "profile.html" 
+
+class LogInPageView(TemplateView):
+  template_name = "login.html"
+
+class LoggedInPageView(TemplateView):
+  template_name = "loggedin.html"
+
+def signIn(request):
+  return render(request, 'login.html')
+
+
+def postSignIn(request):
+  email=request.POST.get('email')
+  passw=request.POST.get('password')
+  try:
+    user=auth.sign_in_with_email_and_password(email,passw)
+  except:
+    message="invalid credentials"
+    return render(request, "login.html",{"messg":message})
+  print(user)
+  return render(request, "loggedin.html",{"e":email})
+
+'''
+def view_name(request):
+  if request.method == "POST":
+    existingEmail = request.POST.get("handle", None)
+    print(existingEmail)
+  return render(request, "login.html")
+
+    try:
+      auth.sign_in_with_email_and_password(existingEmail,existingPassword)
+      print("Successful log in!")
+    except:
+      print("Invalid email or password. Try again.")
+'''
