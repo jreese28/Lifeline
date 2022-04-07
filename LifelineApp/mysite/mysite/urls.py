6caedcd4fe82b1e617669c50a47acbafc47333ba
soliseyoum <56610159+soliseyoum@users.noreply.github.com>
2022-04-07 17:55:15 -0400

    Update urls.py

--- LifelineApp/mysite/mysite/urls.py
+++ LifelineApp/mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf.urls import include
 from main import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main.urls")),
