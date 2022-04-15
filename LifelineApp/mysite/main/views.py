7026bff7f35c01b396cb45f993ef504dd7450bbe
soliseyoum <56610159+soliseyoum@users.noreply.github.com>
2022-04-15 02:19:33 -0400

    Update views.py

--- LifelineApp/mysite/main/views.py
+++ LifelineApp/mysite/main/views.py
@@ -3,10 +3,10 @@ from webbrowser import get
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-import pyrebase
 from django.contrib import auth
 from django.contrib.auth import logout
-
+import pyrebase
+from datetime import datetime
 
 firebaseConfig = {
   "apiKey": "AIzaSyD3b8GXxpcjGhI-2nBVQM2hu_3JUpLj-uM",
@@ -20,21 +20,22 @@ firebaseConfig = {
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
-
 db = firebase.database()
 autho = firebase.auth()
 storage = firebase.storage()
 
+
+##### --------- VIEW PAGE FUNCTIONS ---------- ######
+
+
+
 def index(request):
-  #accessing our firebase data and storing it in a variable
+  # home page view of medicine
   medicine_list = []
   medicine_dict = {}
 
-  #print(udata['localId'])
   uid = request.session['uid']
-  #user_meds = db.child("people").child(uid).child("prescriptions").get()
   user_meds = db.child("people").child(uid).child("presc").get()
-  print("user_meds key:   " + user_meds.key()) # presc
   user_meds_dict = user_meds.val() # OrderedDict([('medsID', ['m1', 'm2'])])
   if user_meds_dict is not None:
     user_meds_dict = user_meds_dict.values() # odict_values([['m1', 'm2', 'm3']])
@@ -42,10 +43,7 @@ def index(request):
     user_meds_list = list(user_meds_dict)[0] # ['m1', 'm2', 'm3']
 
     for med in user_meds_list:
-      print("med" + med)
       med_info = db.child("medicine").child(str(med)).get()
-      print( " med_ info key " + med_info.key())
-      print(med_info.val())
       medicine_dict[med_info.key()] = med_info.val()
   
   medicine_list = medicine_dict.values()
@@ -55,53 +53,48 @@ def index(request):
   }
   return render(request, 'index.html', context)
   
-'''
-    med_info = med.val()
-    if med_info is not None:
-      medicine_dict[med.key()] = med_info
-  medicine_list = medicine_dict.values()
-'''
-
-'''
-users_medicines = db.child("people").child(uid).child("prescriptions").get() # in this case would be m1
-for user_med in users_medicines.each():
-
-  users_med_info =  db.child("medicine").child(users_medicines).get() 
-
-added_med_id = db.child("medicine").get()
-for med in added_med_id.each():
-  if med.val()['name'] == selectedToAdd:
-    med_id = med.key()
-    db.child("people").child(uid).update({"prescriptions": med_id})
-'''
-
 
 
 def add(request):
-  selected=request.POST.get('selectedToAdd')
-  #print(dict(request.POST))
-  #print(selected)
+  # manage page function to add medicine
+  selected=request.POST.get('selectedMed')
   added_med_id = db.child("medicine").get()
   for med in added_med_id.each():
-    #print("key: "+ med.key())
-    #print("val: "+ med.val()['name'])
     if med.val()['name'] == selected:
       med_id = med.key()
-      #db.child("people").child(request.session['uid']).child("prescriptions").update({"med_id": med_id})
       db.child("people").child(request.session['uid']).child("presc").child("medsID").update({ med_id: med_id})
-      #print("updated!")
   return render(request, 'add.html')
 
 
+def deleteMed(request):
+  # manage page function to delete medicine
+  selected=request.POST.get('selectedMed')
+  print(selected)
+  meds_id = db.child("medicine").get()
+  for med in meds_id.each():
+    if med.val()['name'] == selected:
+      med_id = med.key()
+  med_id = med_id
+  trytest = db.child("people").child(request.session['uid']).child("presc").child("medsID").child(med_id).get() 
+  if trytest.val() is None:
+    message= selected + "is not in your medicine list."
+    return render(request, "add.html",{"messg":message}) 
+  db.child("people").child(request.session['uid']).child("presc").child("medsID").child(med_id).remove() 
+  return index(request)
+
+
 def signIn(request):
+  #display log in page upon entering app
   return render(request, 'login.html')
 
 
 def postSignIn(request):
+  # logs user in and fetches user's data from database
   email=request.POST.get('email')
   passw=request.POST.get('password')
   try:
     user=autho.sign_in_with_email_and_password(email,passw)
+    
   except:
     message="Invalid log in credentials."
     return render(request, "login.html",{"messg":message})
@@ -112,6 +105,7 @@ def postSignIn(request):
 
 
 def logOut(request):
+  # ends user session and returns to log in page
   try:
       del request.session['uid']
       auth.logout(request)
@@ -121,6 +115,7 @@ def logOut(request):
 
 
 def postSignUp(request):
+  # creates user in the firebase authenticaton and the database
   email=request.POST.get('newemail')
   password=request.POST.get('newpassword')
   try:
@@ -133,52 +128,40 @@ def postSignUp(request):
   request.session['uid'] = uid
   db.child("people").child(request.session['uid']).update({'email':email})
 
-
   context = {
       "email":email
   }
   return render(request, 'profile.html', context)
 
 
-def register(request):
-  print( "in register:  " +request.session['uid'])
-  name=request.POST.get('fullname')
-  print("REGISTRATION NAME " + name)
-  birthday=request.POST.get('birthday')
-  print("REGISTRATION birthday " + birthday)
-  db.child("people").child(request.session['uid']).update({'name':name})
-  print("1")
-  db.child("people").child(request.session['uid']).update({'birthday':birthday})
-  print("2")
-  #return render(request, 'home.html')
-  return index(request)
 
+
+def register(request):
+  # prompts user to populate profile info and stores it in database
+  name=request.POST.get('fullname')
+  birthday=request.POST.get('birthday')
+  phone=request.POST.get('phone')
+  db.child("people").child(request.session['uid']).update({'name':name})
+  db.child("people").child(request.session['uid']).update({'birthday':birthday})
+  db.child("people").child(request.session['uid']).update({'phone':phone})
+  #return render(request, 'home.html')
+  return add(request)
 
 def profile(request):
   user_info = db.child("people").child(request.session['uid']).get()
   user_info = user_info.val()
+  bday = user_info['birthday']
+  bday = datetime.strptime(user_info['birthday'], '%Y-%m-%d')
 
-
-
+  bday = bday.strftime("%b %d %Y")
+  user_info['birthday'] = bday
 
   context = {
       "user_info":user_info
   }
   return render(request, 'profile.html', context)
-class HomePageView(TemplateView):
-  template_name = "index.html"
 
-class AddPageView(TemplateView):
-  template_name = "add.html" 
 
-class ProfilePageView(TemplateView):
-  template_name = "profile.html" 
-
-class LogInPageView(TemplateView):
-  template_name = "login.html"
-
-class LoggedInPageView(TemplateView):
-  template_name = "loggedin.html"
-
+# shows description page
 class DescriptionPageView(TemplateView):
   template_name = "description.html"
